1b2a4edbf4a8e1cc60c0c0cfeb4f885e4706b145
Your Name <dicomitis13@gmail.com>
2017-03-08 11:43:53 -0700

    reworking Hobbies.js to be more react like

--- src/Hobbies.js
+++ src/Hobbies.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import List from './List'
+import List from './List';
 
 var Hobbies = React.createClass({
   render: function() {
     return (
       <div className="container my-cont">
         <h4 className="generalinfo"> Hello, my name is Taryn. Here is a page all about me! </h4>
-        <ul>
-          <h3> Hobbies </h3>
-          <ListItem item= {"Hiking"} />
-          <li> Skiing </li>
-          <li> Traveling </li>
-          <li> Playing Video Games </li>
-          <li> Eating </li>
-          <li> Drinking Wine/Cider </li>
-        </ul>
+        <h3> Hobbies </h3>
+       <List data= {["Hiking", "Skiing", "Traveling", "Playing Video Games", "Eating", "Drinking Wine/Cider"]} />
       </div>
     )
   }
